ddb100821268bd206ac63c1ec5af4035d3616787
Solomakhin Anton <s.anton45@yandex.ru>
2022-05-17 15:41:29 +0300

    v 1.0

--- src/main/java/collectionGenerate/Data.java
+++ src/main/java/collectionGenerate/Data.java
@@ -7,7 +7,7 @@ import java.util.List;
 public class Data {
 
     // Название репозитория сервиса - так будет называться коллекция
-    public static final String repoServiceName = "TEMP-rpu-common-person.FlSnilsInfoService";
+    public static final String repoServiceName = "rpu-common-person.FlSnilsInfoService";
 
     // Адресс сервиса из метадаты
     public static final String serviceAddress = "ru.gov.pfr.ecp.ebr.rpu.common.fl.SnilsInfoService";
@@ -26,20 +26,15 @@ public class Data {
             "  .google.protobuf.Int64Value dic_ils_cancelation_id = 6;\n" +
             "  .google.protobuf.Int64Value dic_ils_status_id";
 
-    // Обязательные поля для Create
-    public static final List<String> requiredFields = new ArrayList<>(Arrays.asList(
-            "fl_base_data_id", "snils", "dic_ils_status_id"
-    ));
-
-    // Если Get-запрос у сервиса существует - ставим true (в тестах сгенерируется запрос в БД через Get + тесты на Get). В противном случае - false
+    // Если Get-запрос у сервиса существует - ставим true (в тестах сгенерируются тесты на Get). В противном случае - false
     public static boolean getRequestExists = true;
 
-    // Если ставить true - будет адрес localhost:12044, в противном случае - host.docker.internal
+    // Если ставить true - будет адрес localhost:12044, в противном случае - host.docker.internal (в адресной строке постмана)
     public static boolean isLocalhost = true;
 
-    // Если true - то в методе Get запрос с полем id ({"id": ""}), если false - то с value ({"value": ""})
+    // Если true - то в методе Get запрос с полем id ({"id": {"value": ""}"}), если false - то просто с value без id ({"value": ""})
     public static boolean isIdInGetRequest = false;
 
-    // Если true - то в методе Delete запрос с полем id ({"id": ""}), если false - то с value ({"value": ""})
+    // Если true - то в методе Delete запрос с полем id ({"id": {"value": ""}"}), если false - то просто с value без id ({"value": ""})
     public static boolean isIdInDeleteRequest = false;
 }
